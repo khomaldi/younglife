3bc626286518042638361b109f2f6b6ca62106e1
Georgii <khomaldi@yandex.ru>
2019-02-14 04:11:30 +0400

    Update main.php
    added description

--- core/main.php
+++ core/main.php
@@ -1,3 +1,14 @@
 <?php
+
+/**
+ * Created by Visual Studio Code.
+ * @author: Giorgi Khomaldishvili <khomaldi@yandex.ru> <https://github.com/khomaldi>
+ * @copyright Copyright (c) 2019, Giorgi Khomaldishvili
+ * @license none
+ * Date: 13.02.2019
+ * 
+ * This script changes the language based on the page address.
+ */
+
 $rootFolder = $_SERVER['DOCUMENT_ROOT'] . '/';
 ?>
