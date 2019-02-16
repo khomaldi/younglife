417668fdc4aafa627da9bd3252c0e691383650ea
Georgii <khomaldi@yandex.ru>
2019-02-16 09:21:18 +0400

    Stable 1

--- core/change-language.php
+++ core/change-language.php
@@ -5,27 +5,27 @@
  * @author: Giorgi Khomaldishvili <khomaldi@yandex.ru> <https://github.com/khomaldi>
  * @copyright Copyright (c) 2019, Giorgi Khomaldishvili
  * @license none
- * Date: 13.02.2019
+ * Date: 13.02.2019 
  * 
+ * @param string $link
+ * @param string $result
+ * @return string $russian (or $georgian)
  * This script changes the language based on the page address.
  */
 
 $link = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-if (mb_stripos($link, '/ru/') == true) {
-  $georgian = implode(explode('ru/', $link));
+if (mb_stripos($link, '.ge/ru') == false) {
+  $russian = explode('.ge', $link);
+  $russian = $russian[0] . '.ge/ru' . $russian[1];
 } else {
-  $georgian = $link;
+  $russian = $link;
 }
 
-if (mb_stripos($link, '/ru') == false) {
-  $russian = explode('e/', $link);
-  $russian = $russian[0] . 'e/ru/' . $russian[1];
+if (mb_stripos($link, '.ge/ru') == true) {
+  $georgian = explode('.ge/ru', $link);
+  $georgian = $georgian[0] . '.ge' . $georgian[1];
 } else {
-  if ($link == 'https://younglife.ge/ru/') {
-    $russian = 'https://younglife.ge/ru';
-  } else {
-    $russian = $link;
-  }
+  $georgian = $link;
 }
 ?>
