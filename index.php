f61159fc6005d34c465f35b8ff3214b87a4d5c80
Georgii <khomaldi@yandex.ru>
2019-02-17 04:15:03 +0400

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ require_once('core/main.php');
   <meta name="keywords" content="YoungLife, young, life, georgia, საქართველო, იანგ, ლაიფი, იანგლაიფი, ოფიციალური, საიტი">
   <meta name="description" content="«Young Life» &mdash; არის ორგანიზაცია რომელიც შედგება ერთი მიზნით გაერთიანებული ხალხისგან. მათი მთავარი მიზანი მოზარდების ცხოვრების გაუმჯობესებაა. ჩვენ ვეხმარებით ახალგაზრდებს საკუთარი ნიჭის აღმოჩენასა და განვითარებაში. ვასწავლით მათ, თავდაჯერებულად შეხვდნენ ყველა იმ გამოწვევას, რომლებსაც მათ ცხოვრება უგზავნის; გაუმკლავდნენ ყველა გამოწვევას, თავი აიმაღლონ იმედგაცრუებისა და სიბოროტის დასაძლევად; მიაღწიონ ყველა რეალისტურ ოცნებას; ისწავლონ საკუთარი თავის და გარშემომყოფების პატივისცემა.">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta property="og:title" content="Young Life საქართველო" />
-  <meta property="og:image" content="https://younglife.ge/img/big_logo_og.jpg" />
+  <meta property="og:title" content="Young Life საქართველო">
+  <meta property="og:image" content="https://younglife.ge/img/big_logo_og.jpg">
 
   <!-- Favicon -->
-  <link rel="shortcut icon" href="https://younglife.ge/images/favicon.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="https://younglife.ge/images/favicon.ico" type="image/x-icon">
 
   <!-- CSS Files -->
   <link href="/css/younglife.css" rel="stylesheet">
@@ -24,6 +24,7 @@ require_once('core/main.php');
   <link href="/blocks/footer/footer.css" rel="stylesheet">
   <link href="/blocks/map/map.css" rel="stylesheet">
   <link href="/blocks/banners/banners.css" rel="stylesheet">
+  <link href="/blocks/about/about.css" rel="stylesheet">
 
   <!-- JavaScript Files -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -60,6 +61,16 @@ try {
 try {
   if (!@include_once($rootFolder . 'blocks/banners/banners.tpl')) {
     throw new Exception('Can\'t open file &lt;banners.tplgt;');
+  }
+ } catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
+<?php
+//Try to inclide <about.tpl>
+try {
+  if (!@include_once($rootFolder . 'blocks/about/about.tpl')) {
+    throw new Exception('Can\'t open file &lt;about.tplgt;');
   }
  } catch (Exception $e) {
     echo $e->getMessage();
