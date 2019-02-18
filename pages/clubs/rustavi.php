a0ba9e8d3470cd5a68ddc55c918b4aa763c3e362
Georgii <khomaldi@yandex.ru>
2019-02-18 21:18:00 +0400

    Create rustavi.php

---
+++ pages/clubs/rustavi.php
@@ -0,0 +1,64 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/core/main.php');
+?>
+<!DOCTYPE html>
+<html lang="ka">
+
+<head>
+  <!-- Title -->
+  <title>რუსთავი</title>
+
+  <!-- Meta -->
+  <meta charset="utf-8">
+  <meta name="keywords" content="რუსთავი, Rustavi, YoungLife, young, life, younglife, georgia, საქართველო, იანგ, ლაიფი, იანგლაიფი, ოფიციალური, საიტი">
+  <meta name="description"
+    content="Young Life-ის კლუბი რუსთავში. რუსთავის კლუბი. მისამართი: მესხიშვილის მე-2 გასასვლელი, მე-8 კორპუსის მიმდებარე ტერიტორია. დრო: შაბათი 16:00. კონტაქტი: +995-598-36-96-97">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:title" content="Young Life რუსთავი" />
+  <meta property="og:image" content="https://younglife.ge/images/og_logo.jpg" />
+
+<?php
+//Try to include <includes-head.tpl>
+try {
+  if (!@include_once($rootFolder . 'includes/includes-head.tpl')) {
+    throw new Exception('Can\'t open file &lt;includes-head.tpl&gt;');
+  }
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+?>
+
+</head>
+<body>
+<?php
+//Try to include <includes-before-content.php>
+try {
+  if (!@include_once($rootFolder . 'includes/includes-before-content.php')) {
+    throw new Exception('Can\'t open file &lt;includes-before-content&gt;');
+  }
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+?>
+<?php
+//Try to inclide <rustavi.tpl>
+try {
+  if (!@include_once($rootFolder . 'blocks/clubs/rustavi/rustavi.tpl')) {
+    throw new Exception('Can\'t open file &lt;rustavi.tplgt;');
+  }
+ } catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
+<?php
+//Try to include <includes-after-content.php>
+try {
+  if (!@include_once($rootFolder . 'includes/includes-after-content.php')) {
+    throw new Exception('Can\'t open file &lt;includes-after-content&gt;');
+  }
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+?>
+</body>
+</html>
