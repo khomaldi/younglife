a1dbd83829329e4899144c332c8c893f1c05b9e1
Georgii <39419187+khomaldi@users.noreply.github.com>
2019-03-29 00:45:34 +0400

    Update main.php

--- core/main.php
+++ core/main.php
@@ -3,7 +3,7 @@
 /**
  * Created by Visual Studio Code.
  * @author: Giorgi Khomaldishvili <khomaldi@yandex.ru> <https://github.com/khomaldi>
- * @copyright Copyright (c) 2019, Giorgi Khomaldishvili
+ * @copyright Copyright (c) 2019, Georgii Khomaldishvili
  * @license none
  * Date: 13.02.2019
  * 
