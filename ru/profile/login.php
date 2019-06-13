a250c4a31203b5f2789d7fe0d4ae83e2b3e934ec
Georgii <khomaldi@yandex.ru>
2019-06-13 04:26:48 +0400

    Create login.php

---
+++ ru/profile/login.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+
+/* If user has been authorized */
+if (isset($_SESSION['logged_user'])) {
+  echo "<script>self.location='https://younglife.ge/ru/profile/profile.php';</script>";
+} else { }
+
+require_once($_SERVER['DOCUMENT_ROOT'] . 'ru/profile/core/main.php');
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <!-- Title -->
+    <title>Вход</title>
+    <link rel="alternate" hreflang="ka" href="https://younglife.ge/profile/login.php" />
+    <link rel="alternate" hreflang="ru" href="https://younglife.ge/ru/profile/login.php" />
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="keywords" content="Вход, авторизация, YoungLife, young, life, georgia">
+    <meta name="description" content="Вход на сайт YoungLife.ge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="Young Life Грузия">
+    <meta property="og:image" content="https://younglife.ge/images/og_logo.jpg">
+
+    <?php
+    //Try to include <includes-head.html>
+    try {
+      if (!@include_once($rootFolder . 'includes/includes-head.html')) {
+        throw new Exception('Can\'t open file &lt;includes-head.html&gt;');
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+    ?>
+    
+</head>
+
+<body>
+    <?php
+ //Try to include <includes-before-content.php>
+    try {
+      if (!@include_once($rootFolder . 'ru/includes/includes-before-content.php')) {
+        throw new Exception('Can\'t open file &lt;includes-before-content&gt;');
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+    ?>
+    <?php
+ //Try to include <login.html>
+    try {
+      if (!@include_once($rootFolder . 'ru/profile/blocks/login/login.html')) {
+        throw new Exception('Can\'t open file &lt;login.html&gt;');
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+    ?>
+    <?php
+ //Try to include <includes-after-content.php>
+    try {
+      if (!@include_once($rootFolder . 'ru/includes/includes-after-content.php')) {
+        throw new Exception('Can\'t open file &lt;includes-after-content&gt;');
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+    ?>
+</body>
+</html>
+ 
