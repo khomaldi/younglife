afce55dc4706a72954e8a22b3cf5752ff51e1a55
Georgii <khomaldi@yandex.ru>
2019-06-13 04:30:44 +0400

    Update includes-after-content.php

--- includes/includes-after-content.php
+++ includes/includes-after-content.php
@@ -2,8 +2,8 @@
 
 //Try to include <footer.html>
 try {
-  if (!@include_once($rootFolder . 'blocks/footer/footer.html')) {
-    throw new Exception('Can\'t open file <footer.html>');
+  if (!@include_once($_SERVER['DOCUMENT_ROOT'] . 'blocks/footer/footer.html')) {
+    throw new Exception('Can\'t open file &lt;footer.html&gt;');
   }
 } catch (Exception $e) {
   echo $e->getMessage();
